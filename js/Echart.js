5a6effdc0b9cce88e50039b81681845fdbfebf24
yyb714059651 <714059651@qq.com>
2017-11-07 21:27:31 +0800

    响应式后台界面

---
+++ js/Echart.js
@@ -0,0 +1,526 @@
+//111
+ var gg01=echarts.init(document.getElementById("gg01"),'walden');
+ option = {
+    title : {
+        //text: '某地区蒸发量和降水量',
+        //subtext: '纯属虚构'
+    },
+    tooltip : {
+        trigger: 'axis'
+       
+    },
+    legend: {
+        data:['SiteA','SiteB']
+    },
+    toolbox: {
+        show : true,
+        feature : {
+            dataView : {show: true, readOnly: false},
+            magicType : {show: true, type: ['line', 'bar']},
+            restore : {show: true},
+            saveAsImage : {show: true}
+        }
+    },
+    calculable : true,
+    xAxis : [
+        {
+            type : 'category',
+            data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
+        }
+    ],
+    yAxis : [
+        {
+            type : 'value'
+        }
+    ],
+    series : [
+        {
+            name:'SiteA',
+            type:'bar',
+            data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
+            markPoint : {
+                data : [
+                    {type : 'max', name: '最大值'},
+                    {type : 'min', name: '最小值'}
+                ]
+            },
+            markLine : {
+                data : [
+                    {type : 'average', name: '平均值'}
+                ]
+            }
+        },
+        {
+            name:'SiteB',
+            type:'bar',
+            data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
+            markPoint : {
+                data : [
+                    {name : '年最高', value : 182.2, xAxis: 7, yAxis: 183},
+                    {name : '年最低', value : 2.3, xAxis: 11, yAxis: 3}
+                ]
+            },
+            markLine : {
+                data : [
+                    {type : 'average', name : '平均值'}
+                ]
+            }
+        }
+    ]
+};
+gg01.setOption(option);
+
+//第二种   hichsrt
+// var gg01=echarts.init(document.getElementById("gg01"));
+//   gg01.setOption(option);
+//
+//	window.addEventListener("resize",function(){
+//	    var divwidth=parseInt($("#gg01").width());
+//	    var divheight=parseInt($("#gg01").height());
+//	    divheight=divwidth/1.16;
+//	    $("#gg01").height(divheight);
+//	    gg01.resize();
+//	});
+//第三种
+if(window.addEventListener)
+     {
+     window.addEventListener("resize",gg01.resize,false);
+     }
+     else
+     {
+     window.attachEvent("onresize",gg01.resize)
+     }
+//第4 时间设计
+//setTimeout(function(){
+//
+//　　　window.onresize = function(){
+//
+//　　　　　　gg01.resize();
+//　　　}
+//
+//},500);
+
+
+//22
+ var gg02=echarts.init(document.getElementById("gg02"),'dark');
+ option02 = {
+    title: {
+        text: '未来一周气温变化',
+        //subtext: '纯属虚构'
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['最高气温','最低气温']
+    },
+    toolbox: {
+        show: true,
+        feature: {
+            dataZoom: {
+                yAxisIndex: 'none'
+            },
+            dataView: {readOnly: false},
+            magicType: {type: ['line', 'bar']},
+            restore: {},
+            saveAsImage: {}
+        }
+    },
+    xAxis:  {
+        type: 'category',
+        boundaryGap: false,
+        data: ['周一','周二','周三','周四','周五','周六','周日']
+    },
+    yAxis: {
+        type: 'value',
+        axisLabel: {
+            formatter: '{value} °C'
+        }
+    },
+    series: [
+        {
+            name:'最高气温',
+            type:'line',
+            data:[11, 11, 15, 13, 12, 13, 10],
+            markPoint: {
+                data: [
+                    {type: 'max', name: '最大值'},
+                    {type: 'min', name: '最小值'}
+                ]
+            },
+            markLine: {
+                data: [
+                    {type: 'average', name: '平均值'}
+                ]
+            }
+        },
+        {
+            name:'最低气温',
+            type:'line',
+            data:[1, -2, 2, 5, 3, 2, 0],
+            markPoint: {
+                data: [
+                    {name: '周最低', value: -2, xAxis: 1, yAxis: -1.5}
+                ]
+            },
+            markLine: {
+                data: [
+                    {type: 'average', name: '平均值'},
+                    [{
+                        symbol: 'none',
+                        x: '90%',
+                        yAxis: 'max'
+                    }, {
+                        symbol: 'circle',
+                        label: {
+                            normal: {
+                                position: 'start',
+                                formatter: '最大值'
+                            }
+                        },
+                        type: 'max',
+                        name: '最高点'
+                    }]
+                ]
+            }
+        }
+    ]
+};
+	gg02.setOption(option02);
+	setTimeout(function(){
+	　　　window.onresize = function(){
+	
+	　　　　　　gg02.resize();
+	　　　}
+	
+	},500);
+
+//3
+var gg03=echarts.init(document.getElementById("gg03"),'westeros');
+ option03 = {
+    title : {
+        text: '南丁格尔玫瑰图',
+        //subtext: '纯属虚构',
+        x:'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        x : 'center',
+        y : 'bottom',
+        data:['rose1','rose2','rose3','rose4','rose5','rose6','rose7','rose8']
+    },
+    toolbox: {
+        show : true,
+        feature : {
+            mark : {show: true},
+            dataView : {show: true, readOnly: false},
+            magicType : {
+                show: true,
+                type: ['pie', 'funnel']
+            },
+            restore : {show: true},
+            saveAsImage : {show: true}
+        }
+    },
+    calculable : true,
+    series : [
+        {
+            name:'半径模式',
+            type:'pie',
+            radius : [20, 110],
+            center : ['25%', '50%'],
+            roseType : 'radius',
+            label: {
+                normal: {
+                    show: false
+                },
+                emphasis: {
+                    show: true
+                }
+            },
+            lableLine: {
+                normal: {
+                    show: false
+                },
+                emphasis: {
+                    show: true
+                }
+            },
+            data:[
+                {value:10, name:'rose1'},
+                {value:5, name:'rose2'},
+                {value:15, name:'rose3'},
+                {value:25, name:'rose4'},
+                {value:20, name:'rose5'},
+                {value:35, name:'rose6'},
+                {value:30, name:'rose7'},
+                {value:40, name:'rose8'}
+            ]
+        },
+        {
+            name:'面积模式',
+            type:'pie',
+            radius : [30, 110],
+            center : ['75%', '50%'],
+            roseType : 'area',
+            data:[
+                {value:10, name:'rose1'},
+                {value:5, name:'rose2'},
+                {value:15, name:'rose3'},
+                {value:25, name:'rose4'},
+                {value:20, name:'rose5'},
+                {value:35, name:'rose6'},
+                {value:30, name:'rose7'},
+                {value:40, name:'rose8'}
+            ]
+        }
+    ]
+};
+
+	gg03.setOption(option03);
+	if(window.addEventListener)
+     {
+     window.addEventListener("resize",gg03.resize,false);
+     }
+     else
+     {
+     window.attachEvent("onresize",gg03.resize)
+     }
+//4
+var gg04=echarts.init(document.getElementById("gg04"));
+option04 = {
+    title: {
+        //text: '基础雷达图'
+    },
+    tooltip: {},
+    legend: {
+        data: ['预算分配（Allocated Budget）', '实际开销（Actual Spending）']
+    },
+    radar: {
+        // shape: 'circle',
+        name: {
+            textStyle: {
+                color: '#fff',
+                backgroundColor: '#999',
+                borderRadius: 3,
+                padding: [3, 5]
+           }
+        },
+        indicator: [
+           { name: '销售（sales）', max: 6500},
+           { name: '管理（Administration）', max: 16000},
+           { name: '信息技术（Information Techology）', max: 30000},
+           { name: '客服（Customer Support）', max: 38000},
+           { name: '研发（Development）', max: 52000},
+           { name: '市场（Marketing）', max: 25000}
+        ]
+    },
+    series: [{
+        name: '预算 vs 开销（Budget vs spending）',
+        type: 'radar',
+        // areaStyle: {normal: {}},
+        data : [
+            {
+                value : [4300, 10000, 28000, 35000, 50000, 19000],
+                name : '预算分配（Allocated Budget）'
+            },
+             {
+                value : [5000, 14000, 28000, 31000, 42000, 21000],
+                name : '实际开销（Actual Spending）'
+            }
+        ]
+    }]
+};
+  gg04.setOption(option04);
+	if(window.addEventListener)
+     {
+     window.addEventListener("resize",gg04.resize,false);
+     }
+     else
+     {
+     window.attachEvent("onresize",gg04.resize)
+     }
+//5
+//app.title = '环形图';
+var gg05=echarts.init(document.getElementById("gg05"));
+ option05 = {
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        x: 'left',
+        data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+    },
+    series: [
+        {
+            name:'访问来源',
+            type:'pie',
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+                normal: {
+                    show: false,
+                    position: 'center'
+                },
+                emphasis: {
+                    show: true,
+                    textStyle: {
+                        fontSize: '30',
+                        fontWeight: 'bold'
+                    }
+                }
+            },
+            labelLine: {
+                normal: {
+                    show: false
+                }
+            },
+            data:[
+                {value:335, name:'直接访问'},
+                {value:310, name:'邮件营销'},
+                {value:234, name:'联盟广告'},
+                {value:135, name:'视频广告'},
+                {value:1548, name:'搜索引擎'}
+            ]
+        }
+    ]
+};
+  gg05.setOption(option05);
+	if(window.addEventListener)
+     {
+     window.addEventListener("resize",gg05.resize,false);
+     }
+     else
+     {
+     window.attachEvent("onresize",gg05.resize)
+     }
+
+
+//66
+var gg06=echarts.init(document.getElementById("gg06"));
+   option06 = {
+    tooltip : {
+        formatter: "{a} <br/>{b} : {c}%"
+    },
+    toolbox: {
+        feature: {
+            restore: {},
+            saveAsImage: {}
+        }
+    },
+    series: [
+        {
+            name: '业务指标',
+            type: 'gauge',
+            detail: {formatter:'{value}%'},
+            data: [{value: 50, name: '完成率'}]
+        }
+    ]
+};
+
+//setInterval(function () {
+//  option.series[0].data[0].value = (Math.random() * 100).toFixed(2) - 0;
+//  gg06.setOption(option, true);
+//},2000);
+
+gg06.setOption(option06);
+
+	if(window.addEventListener)
+   {
+   window.addEventListener("resize",gg06.resize,false);
+   }
+   else
+   {
+   window.attachEvent("onresize",gg06.resize)
+   }
+     
+//77
+var gg07=echarts.init(document.getElementById("gg07"));
+    option07 = {
+    title: {
+        text: '漏斗图',
+        //subtext: '纯属虚构'
+    },
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c}%"
+    },
+    toolbox: {
+        feature: {
+            dataView: {readOnly: false},
+            restore: {},
+            saveAsImage: {}
+        }
+    },
+    legend: {
+        data: ['展现','点击','访问','咨询','订单']
+    },
+    calculable: true,
+    series: [
+        {
+            name:'漏斗图',
+            type:'funnel',
+            left: '10%',
+            top: 60,
+            //x2: 80,
+            bottom: 60,
+            width: '80%',
+            // height: {totalHeight} - y - y2,
+            min: 0,
+            max: 100,
+            minSize: '0%',
+            maxSize: '100%',
+            sort: 'descending',
+            gap: 2,
+            label: {
+                normal: {
+                    show: true,
+                    position: 'inside'
+                },
+                emphasis: {
+                    textStyle: {
+                        fontSize: 20
+                    }
+                }
+            },
+            labelLine: {
+                normal: {
+                    length: 10,
+                    lineStyle: {
+                        width: 1,
+                        type: 'solid'
+                    }
+                }
+            },
+            itemStyle: {
+                normal: {
+                    borderColor: '#fff',
+                    borderWidth: 1
+                }
+            },
+            data: [
+                {value: 60, name: '访问'},
+                {value: 40, name: '咨询'},
+                {value: 20, name: '订单'},
+                {value: 80, name: '点击'},
+                {value: 100, name: '展现'}
+            ]
+        }
+    ]
+};
+
+
+gg07.setOption(option07);
+	if(window.addEventListener)
+     {
+     window.addEventListener("resize",gg07.resize,false);
+     }
+     else
+     {
+     window.attachEvent("onresize",gg07.resize)
+     }
